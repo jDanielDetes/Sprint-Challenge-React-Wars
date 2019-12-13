1885fdbca7850651b85c892207a07b0274a682b8
jDanielDetes <jdanieldetes@gmail.com>
2019-12-13 11:58:45 -0500

    props set up

--- starwars/src/components/CharacterCards.js
+++ starwars/src/components/CharacterCards.js
@@ -5,10 +5,10 @@ import React from 'react'
  const CharacterCards = props => {
     return (
         <div>
-            <h1>Name</h1>
-            <h2>Age</h2>
-            <h3>height</h3>
-            <p>mass</p>
+            <h1>Name: {props.name}</h1>
+            <h2>Birth Year: {props.birth_year}</h2>
+            <h3>height: {props.height}</h3>
+            <p>mass: {props.mass}</p>
 
         </div>
     )
--- starwars/src/components/CharacterData.js
+++ starwars/src/components/CharacterData.js
@@ -1,6 +1,6 @@
 import React,{useState, useEffect} from 'react'
 import axios  from "axios"
-
+import CharacterCards from "./CharacterCards"
 
 
 function CharacterData() {
@@ -21,13 +21,22 @@ function CharacterData() {
           });
       }, []);
 
-      return(
-          <div>
+        return <div></div>
+      {characters.map(character => {
+        return(
+            <CharacterCards
+            Name={character.name}
+            birth_year= {character.birth_year} 
+            height={character.height}
+            mass={character.mass}
+            
+            />
 
-          </div>
-      )
+        )
+      })}
+      
 
-
+ //{name: "Luke Skywalker", height: "172", mass: "77", hair_color: "blond", skin_color: "fair", …}
 
 }
 
