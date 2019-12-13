7c07153ef3332bf0bd7643479fdb5df718e5ad54
jDanielDetes <jdanieldetes@gmail.com>
2019-12-13 12:55:10 -0500

    data is now displaying

--- starwars/src/components/CharacterData.js
+++ starwars/src/components/CharacterData.js
@@ -14,29 +14,40 @@ function CharacterData() {
     
           .then(response => {
             console.log(response);
-            setCharacters(response.data);
+           
+            const charData= response.data.results
+            setCharacters(charData);
+
           })
           .catch(error => {
             console.log("the data was not returned", error);
           });
       }, []);
 
-        return <div></div>
-      {characters.map(character => {
-        return(
+
+      return(
+        <div className = "container">
+            <div className = "people">
+            {characters.map((character)=>{
+                    return(
             <CharacterCards
-            Name={character.name}
+            name={character.name}
             birth_year= {character.birth_year} 
             height={character.height}
             mass={character.mass}
             
             />
+                )
+            })
 
-        )
-      })}
-      
+            }
 
- //{name: "Luke Skywalker", height: "172", mass: "77", hair_color: "blond", skin_color: "fair", …}
+            </div>
+
+    
+        </div>
+    )
+   
 
 }
 
