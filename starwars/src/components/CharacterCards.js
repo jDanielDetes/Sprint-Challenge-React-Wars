ced76125fa4186865cd73835c757fde7406d7a04
jDanielDetes <jdanieldetes@gmail.com>
2019-12-13 11:47:15 -0500

    Data is in console

--- starwars/src/components/CharacterCards.js
+++ starwars/src/components/CharacterCards.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class CharacterCards extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+
+
+ const CharacterCards = props => {
+    return (
+        <div>
+            <h1>Name</h1>
+            <h2>Age</h2>
+            <h3>height</h3>
+            <p>mass</p>
+
+        </div>
+    )
 }
 
 export default CharacterCards
